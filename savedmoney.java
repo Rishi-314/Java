94c9c85b331d65e48a69fcfb55ff639095669e7b
Rishikesh Bhagat <rishikesh2048x@gmail.com>
2025-07-29 23:38:54 +0530

    Create savedmoney.java

---
+++ savedmoney.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        int Num = 30;
+        System.out.print("The Amount of money saved in 30 days: ");
+        System.out.println((Num*(Num+1))/2);
+    }
+}
