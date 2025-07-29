427c038b8056988173e146451c33b4bafed52cec
Rishikesh Bhagat <rishikesh2048x@gmail.com>
2025-07-29 23:37:41 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+public class oddeven {
+    public static void main(String[] args) {
+        int num = 7; // change this value to test with other numbers
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+    }
+}
