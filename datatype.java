5928b8a2a8c8686cd133730316cce93076780479
Rishikesh Bhagat <rishikesh2048x@gmail.com>
2025-07-29 23:32:24 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Aman";
+        int age = 30;
+        String neighbour = "asjancjkwenaufnkxnckasdlwdacsa";
+        boolean isAdult = false;
+        
+        System.out.println(neighbour.length());
+        System.out.println(name);
+        System.out.println(neighbour.substring(0, 10));
+        
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 94;
+        
+        System.out.println(marks[0]);
+    }
+}
